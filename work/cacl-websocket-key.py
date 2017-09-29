65fce7d8013307a0268db3684b908c41d773f6d8
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2017-09-29 17:11:14 +0300

    Introduce cacl-websocket-key.py
    The cacl-websocket-key.py is used to calculated websocket key
    used as a part of server response during websocket handshake process.

---
+++ work/cacl-websocket-key.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# This file is a part of Scripts project
+#
+# Copyright (C) 2017, Alexander Kurbatov <sir.alkurbatov@yandex.ru>
+#
+# Scripts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scripts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Purprose:
+# Calculate websocket server key.
+
+
+from base64 import b64encode
+from hashlib import sha1
+import sys
+
+sha1f = sha1()
+sha1f.update(sys.argv[1] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+print b64encode(sha1f.digest())
+
