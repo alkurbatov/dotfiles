72b2ae22f0419481adee22b688f54ce9fb3d5121
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2017-10-30 13:45:30 +0300

    doc: update copyrights
    * Set proper project name.
    * Set 2018 year everywhere.

--- work/cacl-websocket-key.py
+++ work/cacl-websocket-key.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 #
-# This file is a part of Scripts project
+# This file is a part of Stash project
 #
-# Copyright (C) 2017, Alexander Kurbatov <sir.alkurbatov@yandex.ru>
+# Copyright (C) 2018, Alexander Kurbatov <sir.alkurbatov@yandex.ru>
 #
-# Scripts is free software: you can redistribute it and/or modify
+# Stash is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Scripts is distributed in the hope that it will be useful,
+# Stash is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
