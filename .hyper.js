884805ee55607928ae27e9ee0fef714f50531d64
Alexander Kurbatov <alkurbatov@virtuozzo.com>
2019-02-21 12:10:43 +0300

    hyper: Bring new config changes
    Signed-off-by: Alexander Kurbatov <alkurbatov@virtuozzo.com>

--- .hyper.js
+++ .hyper.js
@@ -105,6 +105,24 @@ module.exports = {
       lightWhite: '#FFFFFF',
     },
 
+    MaterialTheme: {
+      // Set the theme variant,
+      // OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
+      theme: 'Darker',
+
+      // [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
+      // OPTIONS: From 0.1 to 1
+      backgroundOpacity: '1',
+
+      // [Optional] Set the accent color for the current active tab
+      accentColor: '#64FFDA',
+
+      // [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
+      // OPTIONS: 'dark', 'ultra-dark', 'bright'
+      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
+      vibrancy: 'dark'
+    },
+
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
     //
@@ -153,8 +171,9 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     'hypercwd',
+    'hyper-alt-click',
+    'hyper-material-theme',
     'hyper-search',
-    'hyper-solarized-light',
   ],
 
   // in development, you can create a directory under
