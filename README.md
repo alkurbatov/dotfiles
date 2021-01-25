b33ba2f7b46479ca83e7e4cdccfbdf225d3c51eb
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-01-25 18:50:39 +0300

    feat: Introduce the installation script
    To simplify installation on local and distant machines.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Dotfiles
 My configuration files and useful scripts for OS X and Linux.
 
+## Installation
+```bash
+# To install common configuration files on localhost do:
+$ ./install/sh
+
+# To install common configuration files on a remote server do:
+$ ./install.sh root@remote-server-address
+```
+
 ## License
 
 Copyright (c) 2013-2021 Alexander Kurbatov
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# This file is a part of Dotfiles project
+#
+# Copyright (C) 2013-2021, Alexander Kurbatov <sir.alkurbatov@yandex.ru>
+#
+# Dotfiles is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dotfiles is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Purprose:
+# Install common configuration files to the local or remote machine.
+# Regarding usage see the README.md.
+
+CONFIGS=(
+    .config
+    .emacs
+    .emacs.d
+    .tmux.conf
+    .zsh_aliases
+    .zsh_tools
+    .zshenv
+    .zshrc
+)
+
+if [ $# -eq 0 ]; then
+    for i in "${CONFIGS[@]}"; do
+	cp -rvf "${i}" ~/
+    done
+
+    exit 0
+fi
+
+for i in "${CONFIGS[@]}"; do
+    scp -r "${i}" $1:~/
+done
