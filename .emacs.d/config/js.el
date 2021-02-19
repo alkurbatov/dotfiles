c47f35dd4a343cf844011bfc9d4ecd771b1c0740
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-02-19 18:37:14 +0300

    feat(emacs): Setup linter for JS
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/js.el
+++ .emacs.d/config/js.el
@@ -1,5 +1,7 @@
+(use-package add-node-modules-path)
 (use-package js2-mode)
 (use-package json-mode)
+(use-package flycheck)
 
 ;; Replace builtin JavaScript mode.
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
@@ -7,7 +9,18 @@
 
 (add-hook 'js2-mode-hook
   (lambda ()
+    (add-node-modules-path)
+
     (setq indent-tabs-mode nil)
     (setq js2-basic-offset 2)
+
+    ;; Disable js2-mode embedded checker in favor of flycheck.
+    (setq js2-mode-show-parse-errors nil)
+    (setq js2-mode-show-strict-warnings nil)
+
+    ;; Setup flycheck.
+    ;; Do C-c ! s for manual selection of a checker.
+    (flycheck-mode 1)
+    (setq flycheck-checker 'javascript-eslint)
   )
 )
