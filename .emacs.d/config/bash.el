098d269f820034b70c5ba8500560e25bdcdda551
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-03-01 11:05:29 +0300

    feat(emacs): Use shellcheck for bash/sh
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/bash.el
+++ .emacs.d/config/bash.el
@@ -2,5 +2,10 @@
   (lambda ()
     (setq indent-tabs-mode nil)
     (setq tab-width 4)
+
+    ;; Setup flycheck.
+    ;; Do C-c ! s for manual selection of a checker.
+    ;; Do C-c ! v to verify which checkers used and in which order.
+    (add-to-list 'flycheck-checkers 'sh-shellcheck)
   )
 )
