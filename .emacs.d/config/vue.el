8cde447b4a6cbf6ab434607c9abd1a5537ce9e1b
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-03-17 12:47:47 +0300

    feat(emacs): Tweak vue-mode
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/vue.el
+++ .emacs.d/config/vue.el
@@ -1,2 +1,15 @@
 (use-package vue-mode)
-(use-package vue-html-mode)
+
+(add-hook 'vue-mode-hook
+  (lambda ()
+    ;; vue-mode uses the original js-mode, because
+    ;; js2-mode doesn't work well with mmm-mode.
+    (setq indent-tabs-mode nil)
+    (setq js-indent-level 2)
+    (setq vue-html-tab-width 2)
+    (setq css-indent-offset 2)
+
+    ;; Disable ugly regions background color.
+    (set-face-background 'mmm-default-submode-face nil)
+  )
+)
