61850162cdc864e6d10a4a9f14ba54a53288bee6
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-03-18 18:56:32 +0300

    feat(emacs): Enable linter in vue-mode
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/vue.el
+++ .emacs.d/config/vue.el
@@ -1,7 +1,10 @@
+(use-package add-node-modules-path)
 (use-package vue-mode)
 
 (add-hook 'vue-mode-hook
   (lambda ()
+    (add-node-modules-path)
+
     ;; vue-mode uses the original js-mode, because
     ;; js2-mode doesn't work well with mmm-mode.
     (setq indent-tabs-mode nil)
@@ -11,5 +14,8 @@
 
     ;; Disable ugly regions background color.
     (set-face-background 'mmm-default-submode-face nil)
+
+    ;; Enable lintier.
+    (flycheck-add-mode 'javascript-eslint 'vue-mode)
   )
 )
