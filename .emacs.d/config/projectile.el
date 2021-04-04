8c8fdc1a8d87f52756c565713f2ab46607325e2e
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-04-04 19:02:57 +0300

    feat(emacs): Update projectile settings
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/projectile.el
+++ .emacs.d/config/projectile.el
@@ -1,19 +1,17 @@
 (use-package ag)
-(use-package projectile)
 
+;; See the detailed user manual at:
+;; https://docs.projectile.mx/projectile/index.html
+(use-package projectile)
 (projectile-mode +1)
 
 (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
 
-(setq
-  projectile-project-search-path '("~/work/src/git.acronis.com" "~/work/src/github.com/")
-  projectile-auto-discover nil
-)
+(setq projectile-auto-discover nil)
 
-(projectile-register-project-type
-  'npm '("package.json")
-    :project-file "package.json"
-    :compile "npm install"
-    :test "npm test"
-    :run "npm start"
-    :test-suffix ".spec")
+(projectile-register-project-type 'npm '("package.json")
+  :project-file "package.json"
+  :compile "npm install"
+  :test "npm test"
+  :run "npm start"
+  :test-suffix ".spec")
