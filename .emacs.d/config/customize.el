a0787cf7e9c466f46069392be2e82ff33acf9cf2
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-04-19 09:57:12 +0300

    chore(emacs): Sync customize.el
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/customize.el
+++ .emacs.d/config/customize.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(markdown-mode json-mode cmake-font-lock use-package)))
+ '(package-selected-packages '(markdown-mode json-mode cmake-font-lock use-package))
+ '(safe-local-variable-values '((eval prettier-mode t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
