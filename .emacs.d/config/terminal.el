6bf5175d107a6ad1d614170796d831d073613dd3
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-07-22 15:13:47 +0300

    feat(emacs): Support C-w in embedded shell
    C-w deletes word from shell command.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/terminal.el
+++ .emacs.d/config/terminal.el
@@ -1,5 +1,17 @@
+;;; terminal.el --- configuration settings for embedded terminal.
+
+;;; Code:
 (global-set-key (kbd "C-x `") '(lambda ()  (interactive) (ansi-term "/bin/zsh")))
 (setq shell-file-name "/bin/zsh") ;; use zsh shell by default
 
-;; Tweak embedded shell.
+(defun inject-hotkeys-hook ()
+  ;; Support word removal by C-w key.
+  (define-key term-raw-map (kbd "C-w")
+    (lambda () (interactive) (term-send-raw-string "\e\d"))
+    )
+ )
+
+(add-hook 'term-mode-hook 'inject-hotkeys-hook)
 (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt) ;; hide entered passwords
+
+;; terminal.el ends here.
