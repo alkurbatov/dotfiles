b10d0fde500f85a87768bc935acd1012e4dfda90
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-07-29 17:59:06 +0300

    feat(emacs): Improve Python settings
    * Use black instead of autopep8.
    * Disable flymake.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .emacs.d/config/python.el
+++ .emacs.d/config/python.el
@@ -1,6 +1,17 @@
+;;; python.el --- configures Python support.
+
+;;; Code:
 (use-package elpy)
-(use-package py-autopep8)
+(use-package blacken)
 
 (elpy-enable)
 
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(when (load "flycheck" t t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(add-hook 'elpy-mode-hook (lambda ()
+                            (add-hook 'before-save-hook
+                                      'elpy-black-fix-code nil t)))
+
+;; python.el ends here.
