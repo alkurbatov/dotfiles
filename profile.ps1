f4bed89c239472d0e9d42a7364f403194881c4e1
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-10-07 20:09:36 +0300

    feat(powershell): Add git aliases
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- profile.ps1
+++ profile.ps1
@@ -8,6 +8,46 @@ function Remove-Folder-Recursively {
 }
 New-Alias rmr Remove-Folder-Recursively
 
+function Git-Branch {
+    git.exe branch $args
+}
+New-Alias gb Git-Branch
+
+function Git-Checkout {
+    git.exe checkout $args
+}
+New-Alias gco Git-Checkout
+
+function Git-Pull {
+    git.exe pull $args
+}
+Set-Alias -Name gl -Value Git-Pull -Force -Option AllScope
+
+function Git-Diff {
+    git.exe diff
+}
+New-Alias gd Git-Diff
+
+function Git-Diff-Cached {
+    git.exe diff --cached
+}
+New-Alias gdc Git-Diff-Cached
+
+function Git-Push {
+    git.exe push $args
+}
+Set-Alias -Name gp -Value Git-Push -Force -Option AllScope
+
+function Git-Push-Force {
+    git.exe push --force $args
+}
+Set-Alias gpf Git-Push-Force
+
+function Git-Log {
+    git.exe log --graph --pretty=format:"%C(bold red)%h%Creset -%C(bold yellow)%d%Creset %s %C(bold green)(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit
+}
+New-Alias glg Git-Log
+
 function Edit-Profile {
     Start-Emacs $PROFILE
 }
