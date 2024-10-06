e9d8a24903cfa72a54cf1be74ce6495dbc00a54f
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2024-10-06 15:49:45 +0300

    ✏ Fix spelling
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# Purprose:
+# Purpose:
 # Install common configuration files to the local or remote machine.
 # Regarding usage see the README.md.
 
